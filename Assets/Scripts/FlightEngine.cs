111eacafc05e34b0f47bc0d0cd09f3563ea51be0
Jay Péclard <jay.peclard@gmail.com>
2017-11-12 21:52:09 +0100

    first implementation of waypoint generator using physics

---
+++ Assets/Scripts/FlightEngine.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightEngine {
+    //Flugzeugspezifisch
+    public double cw = 0.492;
+    public double ca = 1.48;
+    //Tragfläche
+    public double A = 846;
+    //Masse
+    public double m = 492000;
+    //Schubkraft
+    public double fs = 4*311000;
+
+
+    //Umweltspezifisch
+    public double g = 9.81;
+    //Windgeschwindigkeit
+    public Vector2 v_wind0 = new Vector2(0, 0);
+    //TODO Luftdichte berechnen abhängig Temperatur und Höhe und Luftfeuchtigkeit
+    double roh = 1;
+
+
+    //zeitlich ändernde Parameter
+    private Vector2 v_Flugzeug = new Vector2(0, 0);
+    private float pitch = Mathf.PI / 18;
+
+
+    // Wiederstandskraft
+    Vector2 Fw(Vector2 v_wind)
+    {
+        Vector2 v_windtotal = v_wind0 + v_wind;
+
+        float help = (float)(cw * (roh / 2.0) * A * v_windtotal.magnitude);
+        return new Vector2(help * v_windtotal.x, help * v_windtotal.y);
+    }
+
+    // Auftriebskraft
+    Vector2 Fa(Vector2 v_wind)
+    {
+        Vector2 v = v_wind0 + v_wind;
+
+        float help = (float)(cw * (roh / 2.0) * A * v.magnitude);
+        return new Vector2(help * v.y, -help * v.x);
+    }
+
+    // Schwerkraft
+    Vector2 Fg()
+    {
+        return new Vector2(0, (float)(-m * g));
+    }
+
+    //Schubkraft abgängig vom Pitch beta
+    Vector2 Fs(float beta)
+    {
+        return new Vector2((float)(Mathf.Cos(beta) * fs), (float)(Mathf.Sin(beta) * fs));
+    }
+
+    //effektiver Flugwinkel
+    double gamma()
+    {
+        Vector2 fa = Fa(v_Flugzeug);
+        return Mathf.Atan(-fa.x / fa.y);
+    }
+
+    public List<Waypoint> CalculateWaypoints(int steps, float deltaTime)
+    {
+        List<Waypoint> waypoints = new List<Waypoint>();
+    
+        Vector3 velocity = new Vector3(0f, 0f, 0f);
+
+        Vector3 position = new Vector3(0f, 0f, 0f); 
+        Vector3 rotation = new Vector3(0f, 0f, 0f);
+
+        waypoints.Add(new Waypoint(position, rotation, 0));
+
+        for (int i = 1; i < steps; i++) {
+            var windspeed = new Vector2(-velocity.z, -velocity.y);
+
+            Vector2 resultingforce = Fw(windspeed) + Fa(windspeed) + Fg() + Fs(pitch);
+
+            // prevent sinking
+            if (resultingforce.y < 0) {
+                resultingforce.y = 0;
+            }
+
+            /*Debug.Log("windspeed");
+            Debug.Log(windspeed);
+            Debug.Log("Fw(windspeed)");
+            Debug.Log(Fw(windspeed));
+
+            Debug.Log("Auftrieb");
+            Debug.Log(Fa(windspeed));
+            Debug.Log("Fg");
+            Debug.Log(Fg());
+            Debug.Log("Fs");
+            Debug.Log(Fs(pitch));
+            Debug.Log("Fres t=" + i);
+            Debug.Log(resultingforce);*/
+
+
+
+            velocity = velocity + (new Vector3(0f, resultingforce.y, resultingforce.x) * (float)(1/m) * deltaTime);
+
+            position = position + velocity * deltaTime;
+            Debug.Log("Position x t=" + i);
+            Debug.Log(position);
+            Debug.Log("Velocity x t=" + i);
+            Debug.Log(velocity);
+            waypoints.Add(new Waypoint(position, rotation, i));
+        }
+
+        return waypoints;
+    }
+}
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Simulation : MonoBehaviour {
+    public FlightEngine engine;
     public List<Waypoint> waypoints;
     public Airplane airplane;
     public Path path;
@@ -14,14 +15,18 @@ public class Simulation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        waypoints.Add(new Waypoint(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 0));
+        /* waypoints.Add(new Waypoint(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 0));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 30), new Vector3(0, 0, 0), 300));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 70), new Vector3(-1, 0, 0), 600));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 71), new Vector3(-3, 0, 0), 601));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 72), new Vector3(-7, 0, 0), 602));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 73), new Vector3(-12, 0, 0), 603));
         waypoints.Add(new Waypoint(new Vector3(0, 0, 74), new Vector3(-15, 0, 0), 604));
-        waypoints.Add(new Waypoint(new Vector3(0, 20, 130), new Vector3(0, 0, 0), 800));
+        waypoints.Add(new Waypoint(new Vector3(0, 20, 130), new Vector3(0, 0, 0), 800));*/
+
+        engine = new FlightEngine();
+
+        waypoints = engine.CalculateWaypoints(1500, 0.02f);
 
         airplane = GetComponentInChildren<Airplane>();
         path = GetComponentInChildren<Path>();
