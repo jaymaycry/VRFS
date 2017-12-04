d7ffaa4bf7e8fab53cd8327fa98bdd940367d446
kempfdav <kempfdav@students.zhaw.ch>
2017-12-04 18:41:22 +0100

    slider defined

---
+++ Assets/Scripts/AircraftUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+public class AircraftUI : MonoBehavior {
+	Simulation simulation;
+	Aircraft aircraft;
+
+	// One slider per aircraft parameter 
+	Slider caSlider;
+	Text caValue;
+
+	Slider cwSlider;
+	Text cwValue;
+
+	Slider wingAreaSlider;
+	Text wingAreaValue;
+
+	Slider massSlider;
+	Text massValue;
+
+	Slider maxThrustSlider;
+	Text maxThrustValue;
+
+	Slider enginesSlider;
+	Text enginesValue;
+
+}
